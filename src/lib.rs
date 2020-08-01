f8a97a9660da042547791d1b1a42d69ef2d1b49d
Tom Pridham <pridham.tom@gmail.com>
2020-08-01 17:45:58 -0600

    implemented basic image comparison

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ use image::{
     imageops::overlay, load_from_memory, load_from_memory_with_format, GenericImageView,
     ImageError, ImageFormat, RgbaImage,
 };
+use img_hash::HasherConfig;
 use rand::random;
 use std::path::Path;
 
@@ -11,13 +12,24 @@ pub fn generate_image(image_buffer: &[u8]) -> Result<Vec<u8>, ImageError> {
     let orig = load_from_memory(image_buffer)?;
     let (width, height) = orig.dimensions();
     let mut new_img = RgbaImage::new(width, height);
-    let e = emoji::get_emoji();
+    let hasher = HasherConfig::new().to_hasher();
+    let orig_hash = hasher.hash_image(&orig);
+    let new_hash = hasher.hash_image(&new_img);
+    let mut dist = orig_hash.dist(&new_hash);
 
-    for _ in 0..1000 {
+    for _ in 0..10 {
+        let e = emoji::get_emoji();
         let w: u32 = random::<u32>() % width;
         let h: u32 = random::<u32>() % height;
         let e = load_from_memory_with_format(e, ImageFormat::Png)?;
-        overlay(&mut new_img, &e, w, h)
+        let mut temp_img = new_img.clone();
+        overlay(&mut temp_img, &e, w, h);
+        let temp_hash = hasher.hash_image(&temp_img);
+        let temp_dist = orig_hash.dist(&temp_hash);
+        if dist > temp_dist {
+            new_img = temp_img;
+            dist = temp_dist;
+        }
     }
     let s = new_img.save(Path::new("./g.png"));
     match s {
