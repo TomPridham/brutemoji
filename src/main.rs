2973c3a09033b4f14dbf4a331edeafb65c152fe2
SethPaul <sethcpaul@gmail.com>
2020-08-08 17:06:16 -0600

    hot mess

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ fn main() {
         JPEGEncoder::new(&mut output).encode_image(&img).unwrap();
         let path = Path::new("./g.png");
 
-        let new_img = brutemoji::generate_image(&output, 90_000, false, path);
+        let new_img = brutemoji::generate_image(&output, 10_000, false, path);
         println!("Took {}.{} seconds",
             now.elapsed().as_secs(),
             now.elapsed().subsec_millis());
