8480c7ec71deecb2a29440af0cdae7038c4f197a
SethPaul <sethcpaul@gmail.com>
2020-08-07 21:42:12 -0600

    move to main

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,10 @@
+use image::{jpeg::JPEGEncoder, open};
+use std::path::Path;
+use std::time::Instant;
 
 mod brutemoji;
-#[cfg(test)]
 
-mod tests {
-    #[test]
-    fn it_works() {
-        use image::{jpeg::JPEGEncoder, open};
-        use std::path::Path;
-        use std::time::Instant;
+fn main() {
         let now = Instant::now();
 
         let img = open(Path::new("./assets/georgia.jpg")).unwrap();
@@ -15,11 +12,10 @@ mod tests {
         JPEGEncoder::new(&mut output).encode_image(&img).unwrap();
         let path = Path::new("./g.png");
 
-        let new_img = crate::brutemoji::generate_image(&output, 30_000, false, path);
-        println!("{}", now.elapsed().as_secs());
+        let new_img = brutemoji::generate_image(&output, 100, false, path);
+        println!("Took {} seconds", now.elapsed().as_secs());
         match new_img {
             Ok(_) => println!("OK"),
             Err(e) => println!("{}", e),
         }
-    }
 }
