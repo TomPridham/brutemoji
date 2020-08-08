a8488cbd669810a837e62346ce5bb66e146d9e4b
SethPaul <sethcpaul@gmail.com>
2020-08-08 16:11:33 -0600

    add status readouts

--- src/brutemoji.rs
+++ src/brutemoji.rs
@@ -4,18 +4,30 @@ use rand::thread_rng;
 use std::path::Path;
 // use rand::seq::SliceRandom;
 use rand::prelude::*;
+use std::time::Instant;
 
 mod emoji;
 
+fn measure_dist(rng: &mut ThreadRng, samples_a: &[u8], samples_b: &[u8]) -> i32 {
+    //let mut rng = thread_rng();
+    let sample_length = samples_a.len();
+    let v1 = (1..sample_length).choose_multiple(rng, sample_length/10);
+    let v1_iter = v1.iter();
+    let mut diff = 0;
+    for val in v1_iter {
+        diff = diff + (samples_a[*val] as i32 - samples_b[*val] as i32).abs();
+    }
+
+    return diff;
+}
+
 fn sums_chunked(samples_a: &[u8], samples_b: &[u8]) -> i32 {
-    let rgba = samples_a
+    samples_a
         .chunks_exact(1)
         .zip(samples_b.chunks_exact(1))
-        .fold(0, |rgba, (p_a, p_b)|
+        .fold(0, |rgba, (p_a, p_b)| {
                 rgba + (p_a[0] as i32 - p_b[0] as i32).abs()
-        );
-
-    return rgba;
+        })
 }
 
 pub fn generate_image(
@@ -24,6 +36,8 @@ pub fn generate_image(
     save_progress: bool,
     path: &Path,
 ) -> Result<Vec<u8>, ImageError> {
+    let now = Instant::now();
+
     let mut emoji_cache = emoji::EmojiCache::new();
     let orig = load_from_memory(image_buffer)?.into_rgba();
     let (width, height) = orig.dimensions();
@@ -40,8 +54,12 @@ pub fn generate_image(
         let y: u32 = (1..height).choose(&mut rng).unwrap();
         overlay(&mut new_img, e, x, y);
     }
+
+    dist = sums_chunked(&orig, &new_img);
+    println!("dist is {}", dist);
     let mut placed_count = 0;
-    for _ in 0..iterations {
+    
+    for index in 0..iterations {
         let e = emoji_cache.get_emoji();
         let x: u32 = (1..width).choose(&mut rng).unwrap();
         let y: u32 = (1..height).choose(&mut rng).unwrap();
@@ -55,6 +73,15 @@ pub fn generate_image(
             if save_progress {
                 new_img.save(path)?;
             }
+        }
+        if index%1000==0 {
+            println!("iteration: {}, dist: {}, time: {}.{}, emoji: {}",
+                index,
+                temp_dist,
+                now.elapsed().as_secs(),
+                now.elapsed().subsec_millis(),
+                placed_count
+                )
         }
     }
     println!("placed {} emoji", placed_count);
