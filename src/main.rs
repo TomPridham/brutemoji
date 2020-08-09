85864586d8232391626d8bc7093811cc5e399b7b
SethPaul <sethcpaul@gmail.com>
2020-08-09 10:00:25 -0600

    default args

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,19 @@
-use image::{jpeg::JPEGEncoder, open};
+use image::open;
 use std::path::Path;
 use std::time::Instant;
+use std::env;
 
 mod brutemoji;
 
 fn main() {
         let now = Instant::now();
+        let args: Vec<String> = env::args().collect();
+        let config = Config::new(&args);
+        let img = open(Path::new(&config.filename)).unwrap();
+        let new_path = String::from(format!("./output/{}.png", "1"));
+        let path = Path::new(&new_path);
 
-        // let img = open(Path::new("./assets/hot_tubbie.jpg")).unwrap();
-        let img = open(Path::new("./assets/georgia.jpg")).unwrap();
-        // let mut output = Vec::new();
-        // JPEGEncoder::new(&mut output).encode_image(&img).unwrap();
-        let path = Path::new("./output/g.png");
-
-        let new_img = brutemoji::generate_image(&img, 90_000, false, path);
+        let new_img = brutemoji::generate_image(&img, config.iterations, false, path);
         println!("Took {}.{} seconds",
             now.elapsed().as_secs(),
             now.elapsed().subsec_millis());
@@ -21,4 +21,28 @@ fn main() {
             Ok(_) => println!("OK"),
             Err(e) => println!("{}", e),
         }
+}
+
+struct Config {
+    filename: String,
+    iterations: u64,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let iterations : u64;
+        let filename : String;
+        if args.len() < 2 {
+            iterations = 10_000;
+            filename = String::from("./assets/georgia.jpg");
+        } else if args.len() < 3 {
+            iterations = 10_000;
+            filename = String::from("./assets/georgia.jpg");
+            return Config {filename, iterations }
+        } else {
+        iterations = args[1].clone().parse::<u64>().unwrap();
+        filename = args[2].clone();
+        }
+        Config {filename, iterations }
+    }
 }
